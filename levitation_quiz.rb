54b455f0b8d026097df1c15d4a876e8fe23539dd
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-22 09:06:33 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,5 +1,14 @@
 
 def levitation_quiz
+    loop do 
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
+end
+ 
+annoying
 
 end
 
